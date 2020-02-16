35fd9e3aaefca3af17ac61c113b8ab65a6dbb9fd
Wei-Hang Wang <wf6212@gmail.com>
2020-02-16 22:58:43 +0800

    initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,31 @@
+<template>
+    <nav>
+        <v-toolbar flat app>
+            <v-app-bar-nav-icon @click ="drawer = !drawer"></v-app-bar-nav-icon>
+            <v-toolbar-title class="text-uppercase grey--text">
+                <span class="font-weight-light">Todo</span>
+                <span>Ninja</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-btn depressed>
+                <span>Sign Out</span>    
+                <v-icon right>mdi-exit-to-app</v-icon>
+            </v-btn> 
+        </v-toolbar>
+
+        <v-navigation-drawer app temporary v-model="drawer" class="deep-purple accent-4">
+            <p>test</p>
+        </v-navigation-drawer>
+    </nav>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: false,
+        };
+    }
+}
+</script>
+
